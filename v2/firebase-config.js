342bc6bce2ff6f568fa483addb246d399876757c
mohx94 <mohammadona7@gmail.com>
2026-07-13 15:04:45 +0300

    Albatil Fitness v2.6

---
+++ v2/firebase-config.js
@@ -0,0 +1,8 @@
+window.ALBATIL_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD0fzqyBdOL1aYsp-S2foU3kGoeukRvdrw",
+  authDomain: "albatil-fitness.firebaseapp.com",
+  projectId: "albatil-fitness",
+  storageBucket: "albatil-fitness.firebasestorage.app",
+  messagingSenderId: "403289155833",
+  appId: "1:403289155833:web:c6525d36c3c6be66d61a28"
+};
