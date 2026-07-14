458c5271713b124afd1ce391a74192b0cde9f72d
mohx94 <mohammadona7@gmail.com>
2026-07-14 09:57:57 +0300

    Albatil Fitness v3.0

--- sw.js
+++ sw.js
@@ -1,28 +1,5 @@
-const CACHE='albatil-fitness-v2';
+const CACHE='albatil-fitness-v3-smart-coach';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(Promise.all([
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
-    self.clients.claim()
-  ]));
-});
-self.addEventListener('fetch',e=>{
-  const req=e.request;
-  if(req.mode==='navigate'||req.destination==='document'){
-    e.respondWith(
-      fetch(req).then(res=>{
-        caches.open(CACHE).then(c=>c.put(req,res.clone()));
-        return res;
-      }).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html')))
-    );
-    return;
-  }
-  e.respondWith(caches.match(req).then(r=>r||fetch(req).then(res=>{
-    caches.open(CACHE).then(c=>c.put(req,res.clone()));
-    return res;
-  }).catch(()=>r)));
-});
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
+self.addEventListener('fetch',e=>{const r=e.request;if(r.mode==='navigate'||r.destination==='document'){e.respondWith(fetch(r).then(x=>{caches.open(CACHE).then(c=>c.put(r,x.clone()));return x}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(r).then(x=>x||fetch(r).then(y=>{caches.open(CACHE).then(c=>c.put(r,y.clone()));return y}))) });
